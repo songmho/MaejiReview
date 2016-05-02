ec062ad1c8b926af4ff0eca782c34ffb2151d770
songmho <songmho@gmail.com>
2016-05-02 20:05:36 +0900

    MainActivity에 FAB추가 몇가지 라이브러리 추가

--- app/src/main/java/underflow/underflow/maejireview/MainActivity.java
+++ app/src/main/java/underflow/underflow/maejireview/MainActivity.java
@@ -1,13 +1,27 @@
 package underflow.underflow.maejireview;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab:
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
