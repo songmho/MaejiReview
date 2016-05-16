c66234ff5628350f45fced83cf760a38493819f8
seun2354 <seun2354@naver.com>
2016-05-16 20:38:06 +0900

    회원가입 activity 수정

--- app/src/main/java/underflow/underflow/maejireview/SignInActivity.java
+++ app/src/main/java/underflow/underflow/maejireview/SignInActivity.java
@@ -1,12 +1,33 @@
 package underflow.underflow.maejireview;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
- * Created by Administrator on 2016-05-15.
+ * Created by Administrator on 2016-05-16.
  */
-public class SignInActivity extends AppCompatActivity{
 
 
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        Button signin_in=(Button)findViewById(R.id.signin_in);
+        signin_in.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(SignInActivity.this, "ㅎㅇ", Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/underflow/underflow/maejireview/SignUpActivity.java
@@ -0,0 +1,21 @@
+package underflow.underflow.maejireview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+/**
+ * Created by Administrator on 2016-05-15.
+ */
+public class SignUpActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+
+    }
+}
